70fda76b3e30cacd96c462a57a59509a87f0809b
Jonas Svanberg <jonas.svanberg@gmail.com>
2026-06-16 17:43:20 +0200

    universal-github-app-jwt

--- src/github.ts
+++ src/github.ts
@@ -1,5 +1,12 @@
+import githubAppJwt from 'universal-github-app-jwt'
+
 class GitHub {
     static async initialize(appId, privateKey, organizationSlug, repositorySlug) {
+        const { token } = await githubAppJwt({
+            id: appId,
+            privateKey
+        })
+
         return null
     }
 }
