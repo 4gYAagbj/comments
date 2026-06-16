6b27966d2d8b5f76f5bc5fafef8cb6bac439013d
Jonas Svanberg <jonas.svanberg@gmail.com>
2026-06-16 18:47:42 +0200

    kolla

--- src/github.ts
+++ src/github.ts
@@ -49,6 +49,7 @@ class GitHub {
     }
 
     async getFileFromRepository(filePath, branchFrom = 'master') {
+        console.log(`kolla: https://api.github.com/repos/${this.organizationSlug}/${this.repositorySlug}/contents/${filePath}?ref=${branchFrom}`)
         const fileResponse = await fetch(
             `https://api.github.com/repos/${this.organizationSlug}/${this.repositorySlug}/contents/${filePath}?ref=${branchFrom}`,
             {
