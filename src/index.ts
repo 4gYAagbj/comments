bab6f71c9f2bea73869e970f0da4b7462fa54f47
Jonas Svanberg <jonas.svanberg@gmail.com>
2026-06-16 15:52:46 +0200

    semicolon

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ app.get('/', (c) => {
   return c.text('Hello there!')
 })
 
-app.all('*', () => new Response('These are not the droids you are looking for', { status: 404 }));
+app.all('*', () => new Response('These are not the droids you are looking for', { status: 404 }))
 
 export default app
