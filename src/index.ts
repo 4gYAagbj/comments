0bca36e99fdd73172677cb8bd2b382590bc11b25
Jonas Svanberg <jonas.svanberg@gmail.com>
2026-06-16 16:09:17 +0200

    baseurl

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,12 @@ app.get('/', (c) => {
   return c.text('Hello there!')
 })
 
-app.post('/api/handle/form', async c => c.text('Created', 201))
+app.post('/api/handle/form', async c => {
+  const { req, env } = c
+  const currentUrl = req.url
+  const baseUrl = currentUrl.match(/^(https?:\/\/[^/]+)/)[1]
+  return c.text('Created', 201)
+})
 
 app.all('*', () => new Response('These are not the droids you are looking for', { status: 404 }))
 
