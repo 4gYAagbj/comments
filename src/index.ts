9e888649d9557f9d1cdfd3490c420e5bd531af18
Jonas Svanberg <jonas.svanberg@gmail.com>
2026-06-16 17:30:31 +0200

    GITHUB_REPOSITORY_SLUG

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import GitHub from './github'
 
 // Type definition to make type inference
 type Variables = {
-  GITHUB_APP_ID: string
-  GITHUB_APP_PRIVATE_KEY: string
-  GITHUB_ORGANIZATION_SLUG: string
+  GITHUB_APP_ID: string,
+  GITHUB_APP_PRIVATE_KEY: string,
+  GITHUB_ORGANIZATION_SLUG: string,
+  GITHUB_REPOSITORY_SLUG: string
 }
 
 const app = new Hono<{ Variables: Variables }>()
@@ -17,16 +18,12 @@ app.get('/', (c) => {
   if (matches !== null) {
     const baseUrl = matches[1]
   }
-  console.log('log number 1')
+
   const appId = env.GITHUB_APP_ID
-  console.log('log number 2 x')
   // We need to format the private key to handle the line breaks accordingly
   const formattedPrivateKey = env.GITHUB_APP_PRIVATE_KEY.replace(/\\n/g, '\n')
-  console.log('log number 3')
   const organizationSlug = env.GITHUB_ORGANIZATION_SLUG
-  console.log('log number 4')
   const repositorySlug = env.GITHUB_REPOSITORY_SLUG
-  console.log('log number 5')
   return c.text('Hello you there!')
 })
 
